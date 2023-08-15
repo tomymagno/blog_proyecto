235a59df2767cd0d8cc0e91fb9c4f76773250072
tomymagno <tomasmagnone@gmail.com>
2023-08-15 20:06:00 -0300

    agregados del migrate(error)

--- perfiles/forms.py
+++ perfiles/forms.py
@@ -1,9 +1,14 @@
-# perfiles/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Perfil
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField()
 
 class LoginForm(AuthenticationForm):
     pass
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Perfil
+        fields = ['nombre', 'apellido', 'avatar']
